e29ca38d3ef00c27cbdbac7616fafa593e5be402
mischegoss <mischegoss@gmail.com>
2025-10-10 20:54:07 -0400

    Update src

---
+++ docs-rita-go-current/src/theme/Navbar/Layout/NavigationDropdowns.js
@@ -0,0 +1,129 @@
+import React from 'react'
+import Link from '@docusaurus/Link'
+import styles from './styles.module.css'
+
+export default function NavigationDropdowns() {
+  return (
+    <>
+      {/* Documentation Dropdown */}
+      <div
+        className={`navbar__item dropdown dropdown--hoverable ${styles.dropdown}`}
+      >
+        <a
+          href='#'
+          aria-haspopup='true'
+          aria-expanded='false'
+          role='button'
+          className={`navbar__link ${styles.navbarLink}`}
+        >
+          Documentation
+        </a>
+        <ul className={`dropdown__menu ${styles.dropdownMenu}`}>
+          <li>
+            <Link
+              className={`dropdown__link ${styles.dropdownLink}`}
+              to='/actions'
+            >
+              Actions
+            </Link>
+          </li>
+          <li>
+            <Link className={`dropdown__link ${styles.dropdownLink}`} to='/pro'>
+              Pro
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={`dropdown__link ${styles.dropdownLink}`}
+              to='/express'
+            >
+              Express
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={`dropdown__link ${styles.dropdownLink}`}
+              to='/insights'
+            >
+              Insights
+            </Link>
+          </li>
+        </ul>
+      </div>
+
+      {/* Learning Dropdown */}
+      <div
+        className={`navbar__item dropdown dropdown--hoverable ${styles.dropdown}`}
+      >
+        <a
+          href='#'
+          aria-haspopup='true'
+          aria-expanded='false'
+          role='button'
+          className={`navbar__link ${styles.navbarLink}`}
+        >
+          Training
+        </a>
+        <ul className={`dropdown__menu ${styles.dropdownMenu}`}>
+          <li>
+            <Link
+              className={`dropdown__link ${styles.dropdownLink}`}
+              to='https://training.resolve.io/learning/discover'
+            >
+              Discover Resolve
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={`dropdown__link ${styles.dropdownLink}`}
+              to='https://training.resolve.io/learning'
+            >
+              Learning Hub
+            </Link>
+          </li>
+        </ul>
+      </div>
+
+      {/* Support Dropdown */}
+      <div
+        className={`navbar__item dropdown dropdown--hoverable ${styles.dropdown}`}
+      >
+        <a
+          href='#'
+          aria-haspopup='true'
+          aria-expanded='false'
+          role='button'
+          className={`navbar__link ${styles.navbarLink}`}
+        >
+          Support
+        </a>
+        <ul className={`dropdown__menu ${styles.dropdownMenu}`}>
+          <li>
+            <Link
+              className={`dropdown__link ${styles.dropdownLink}`}
+              to='https://support.resolve.io/'
+            >
+              Support Portal
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={`dropdown__link ${styles.dropdownLink}`}
+              href='/announcement-support-portal'
+            >
+              Support Portal Update
+            </Link>
+          </li>
+        </ul>
+      </div>
+
+      {/* Automation Exchange Link */}
+      <Link
+        className={`navbar__item navbar__link ${styles.navbarLink}`}
+        to='https://exchange.resolve.io/'
+      >
+        Automation Exchange
+      </Link>
+    </>
+  )
+}
